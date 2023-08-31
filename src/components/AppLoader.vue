68d387d782504a4ab8af914f4135c36d261d2f59
AntonelloStefania <antonelloste94@gmail.com>
2023-08-31 09:47:11 +0200

    inserito loader e mostrate a schermo cards con immagine

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,22 @@
+<script>
+    export default {
+        
+    }
+</script>
+<template >
+    <div class="row d-flex loader">
+        <div class="text-center justify-content-center align-self-center ">
+            <div class="spinner-border" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss">
+@keyframes spinner-border {
+    to { transform: rotate(360deg) #{"/* rtl:ignore */"}; }
+}
+.loader{
+    height: 100vh;
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,10 @@
 <script>
 import axios from 'axios';
+import AppLoader from './AppLoader.vue';
     export default {
-        
+        components:{
+            AppLoader,
+        },
         data() {
             return {
                baseUrl: 'http://localhost:8000',
@@ -9,13 +12,16 @@ import axios from 'axios';
                loading: true, 
             }
         },created() {
+            this.getSites();
             
         },methods: {
             getSites(){
                 this.loading=true;
                 axios.get(`${this.baseUrl}/api/sites`).then((response)=>{
+                    console.log(response)
                     if(response.data.success){
                         this.sites = response.data.results;
+                        this.loading=false;
                     }
                     else{
                         //poi vediamo
@@ -27,9 +33,27 @@ import axios from 'axios';
     }
 </script>
 <template>
-    <div class="card">
-        ciao
+    <div class="container">
+        <div class="row">
+            <AppLoader  v-if="loading"/>
+            <div v-else class="col-12 col-md-4 my-2" v-for="site in sites" :key="site">
+                <div class="card">
+                    <div class="card-img-top ">
+                        <img :src="`${baseUrl}/storage/${site.image}`" class="img-fluid w-100" alt="">
+                    </div>
+                    <div class="card-body">
+                        <div class="card-header">
+                            {{ site.title }}
+                        </div>
+                    </div>
+                    <div class="card-text">
+                        {{ site.description }}
+                    </div>
+                 </div>
+            </div>
+        </div>
     </div>
+   
 </template>
 <style lang="scss">
     
